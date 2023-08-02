dde1c252b310d10e8693393ea63c34182b5fcf8d
Omkar.S.Sawant <139449932+Omkar631@users.noreply.github.com>
2023-08-02 22:48:03 +0530

    Add files via upload

---
+++ abstraction/Inf1.java
@@ -0,0 +1,11 @@
+package abstraction;
+
+public interface Inf1 {
+	
+	 void area_of_rectangle();
+	 void area_of_circle();
+	 void area_of_triangle();
+	 void area_of_sqaure();
+	 void area_of_cube();
+
+}
